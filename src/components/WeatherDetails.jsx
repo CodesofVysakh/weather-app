25b53cdc8e9d05b15de615b31e332ba2af3dc120
CodesofVysakh <onvysakh309@gmail.com>
2022-01-10 00:01:32 +0530

    Api Connected

--- src/components/WeatherDetails.jsx
+++ src/components/WeatherDetails.jsx
@@ -1,63 +1,139 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-function WeatherDetails() {
+function WeatherDetails(
+    {temp, humidity, pressure, weatherType, name, speed, country, sunset,}
+) {
+    const [ weatherState, setWeatherState ] = useState("");
+    const now = new Date()
+    const dateString = now.toLocaleDateString();
+    const time = now.toLocaleTimeString();
+
+    let sec = sunset;
+    let date = new Date(sec * 1000);
+    let timeStr = `${date.getHours()}:${date.getMinutes()}`; 
+
+    useEffect(() => {
+        if (weatherType) {
+            switch (weatherType) {
+                case "Clouds":
+                    setWeatherState("wi-day-cloudy");
+                    break;
+                case "Haze":
+                    setWeatherState("wi-fog");
+                    break;
+                case "Clear":
+                    setWeatherState("wi-day-sunny");
+                    break;
+                case "Mist":
+                    setWeatherState("wi-dust");
+                    break;
+                case "Rain":
+                    setWeatherState("wi-day-rain");
+                    break;
+                case "Smoke":
+                    setWeatherState("wi-smoke");
+                    break;
+
+                default:
+                    setWeatherState("wi-day-sunny");
+                    break;
+            }
+        }
+    }, [weatherType])
+
     return (
         <>
             <article className='widget'>
                 <div className='weathericon'>
-                    <i className="wi wi-day-sunny"></i>
+                    {weatherState ? 
+                    <i className={`wi ${weatherState}`}></i>:
+                    <i className={`wi wi-day-sunny`}></i>
+                }
                 </div>
                 <div className="weatherInfo">
                     <div className="temperature">
-                        <span>23.99&deg;</span>
+                        {temp ? 
+                            <span>{temp}&deg;</span> :
+                            <span>0&deg;</span>}
                     </div>
                     <div className="description">
                         <div className="weatherCondition">
-                            Sunny
+                            {weatherType ? 
+                            <h2>{weatherType}</h2> :
+                            <h2> - </h2>}
                         </div>
                         <div className="place">
-                            Mumbai, IN
+                            {name ? 
+                            <h6>{name}, {country}</h6> : 
+                            <h6></h6>}
                         </div>
                     </div>
+                    <div className="date">{dateString} <br /> {time}</div>
                 </div>
-                <div className="date">{new Date().toLocaleString()}</div>
                 <div className="extraTemp">
                     <div className="tempInfoMinMax">
                         <div className="twoSidedsecction">
                             <p>
                                 <i className={"wi wi-sunset"}></i>
                             </p>
+                            {timeStr !== "NaN:NaN" ? 
                             <p className="extraInfoLeftSide">
-                                6:30 PM<br />
-                                Sunset
-                            </p>
+                            {timeStr} PM<br />
+                            Sunset
+                        </p>:
+                        <p className="extraInfoLeftSide">
+                        00:00 <br />
+                        Sunset
+                    </p>}
+                            
                         </div>
+                    </div>
+                    <div className="tempInfoMinMax">
                         <div className="twoSidedsecction">
                             <p>
                                 <i className={"wi wi-humidity"}></i>
                             </p>
+                            {humidity ? 
                             <p className="extraInfoLeftSide">
-                                644<br />
-                                Humidity
-                            </p>
+                            {humidity}<br />
+                            Humidity
+                        </p>:
+                        <p className="extraInfoLeftSide">
+                        0<br />
+                        Humidity
+                    </p>}
                         </div>
+                    </div>
+                    <div className="tempInfoMinMax">
                         <div className="twoSidedsecction">
                             <p>
                                 <i className={"wi wi-rain"}></i>
                             </p>
+                            {pressure ? 
                             <p className="extraInfoLeftSide">
-                                321<br />
-                                Pressure
-                            </p>
+                            {pressure}<br />
+                            Pressure
+                        </p>:
+                        <p className="extraInfoLeftSide">
+                        0<br />
+                        Pressure
+                    </p>}
                         </div>
+                    </div>
+                    <div className="tempInfoMinMax">
                         <div className="twoSidedsecction">
                             <p>
                                 <i className={"wi wi-strong-wind"}></i>
                             </p>
+                            {speed ? 
                             <p className="extraInfoLeftSide">
-                                123<br />
+                                {speed}<br />
                                 Speed
-                            </p>
+                            </p>:
+                            <p className="extraInfoLeftSide">
+                                0<br />
+                                Speed
+                            </p>}
                         </div>
                     </div>
                 </div>
--- src/components/screens/SearchMain.jsx
+++ src/components/screens/SearchMain.jsx
@@ -3,17 +3,29 @@ import WeatherDetails from '../WeatherDetails';
 
 function SearchMain() {
     const [searchTerm, setSearchTerm] = useState("");
+    const [tempInfo, setTempInfo] = useState({})
     console.log(searchTerm, "searchTerm");
     const getWeatherInfo = async () => {
         try {
-            let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=8f26c3866741132a64df6d1f8b82d5d2`;
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&units=metric&appid=8f26c3866741132a64df6d1f8b82d5d2`;
 
             let res = await fetch(url);
             let data = await res.json();
-            console.log(data, "data url");
+            const {temp, humidity, pressure} = data.main;
+            const {main: weatherType} = data.weather[0];
+            const {name} = data;
+            const {speed} = data.wind;
+            const {country, sunset} = data.sys;
+
+            const myNewWeatherInfo = {
+                temp, humidity, pressure, weatherType, name, speed, country, sunset,
+            };
+            setTempInfo(myNewWeatherInfo); 
+            // console.log(data, "data url");
             
         } catch (error) {
-            console.log(error);
+            console.log(error, "error");
+            alert("Enter a valid City Name.")
         }
     }
 
@@ -24,12 +36,13 @@ function SearchMain() {
     return (
         <>
             <div className='wrap'>
+                <h2 className='title'>Weather Forecast</h2>
                 <div className="search">
                     <input type="search" placeholder='Enter City...' id='search' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
                     <button className='searchButton' onClick={getWeatherInfo}>Search</button>
                 </div>
             </div>
-            <WeatherDetails />
+            <WeatherDetails {...tempInfo} />
         </>
     )
 };
